243a8e9f6ea1e9079d2b2b2d03095cfb5e4e3501
buinhathuy511 <huynhatbui123@gmail.com>
2024-12-22 19:58:24 +0700

    NhatHuy | format code

--- frontend/src/pages/todo.js
+++ frontend/src/pages/todo.js
@@ -4,29 +4,8 @@ import Filter from "../components/Filter/Filter";
 import TaskList from "../components/TaskList/TaskList";
 
 function Todo() {
-  // const [tasksList, setTasksList] = useState(() => {
-  //   const storageTasks = JSON.parse(localStorage.getItem("tasks"));
-  //   return storageTasks ?? [];
-  // });
-
-  // const handleAddTask = (task) => {
-  //   setTasksList((prev) => {
-  //     const newTasks = [...prev, task];
-  //     localStorage.setItem("tasks", JSON.stringify(newTasks));
-  //     return newTasks;
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   localStorage.setItem("tasks", JSON.stringify(tasksList));
-  // }, [tasksList]);
-
   return (
     <div>
-      {/* <Header />
-      <AddTask onAddTask={handleAddTask} />
-      <Filter />
-      <TaskList tasks={tasksList} /> */}
       <Header />
       <AddTask />
       <Filter />
